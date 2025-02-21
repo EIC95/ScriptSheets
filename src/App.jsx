ebc23162bc05789654c52cabab3ec6a1f70bf6ed
Ibrahima <ibou9543@gmail.com>
2025-02-21 04:26:21 +0000

    fix not supported routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Navigate, Route, Routes,} from "react-router-dom";
 import Navbar from "./components/navbar/Navbar.jsx";
 import Body from "./components/body/Body.jsx";
 import Home from "./components/home/Home.jsx";
-import './style.css'
+import './style.css';
 
 function App() {
-    return(
+    return (
         <BrowserRouter>
             <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/:tech/:subject" element={<Body />} />
+                <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
         </BrowserRouter>
-    )
+    );
 }
 
-export default App
+export default App;
